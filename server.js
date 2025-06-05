4d90479ba9d01e04c788ff9b8d92a26255679e49
VapeBrat <nmhookah@gmail.com>
2025-06-04 22:12:10 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 const bangers = require('./bangerData.json');
 
 app.get('/bangers', (req, res) => {
